5ce3c74349c6691551e025f18484fef8c7c6874a
boroda74 <rusmusicbee@gmail.com>
2026-02-12 02:27:11 +0300

    * Rebuild

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 //You can specify all the values, or you can default the Build and Revision Numbers 
 //by using the '*' as shown below:
 //[assembly: AssemblyVersion("10.0.*")]
-[assembly: AssemblyVersion("10.1.9538.27572")]
-[assembly: AssemblyFileVersion("10.1.9538.27572")]
+[assembly: AssemblyVersion("10.1.9538.42200")]
+[assembly: AssemblyFileVersion("10.1.9538.42200")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyInformationalVersion("10.1.9538.27572")]
+[assembly: AssemblyInformationalVersion("10.1.9538.42200")]
